0da86550526ba445a0515c4b1ded6e3bf790543b
MavFeuille <mavis.feuille@gmail.com>
2021-11-07 01:05:03 -0500

    remove white space and created solution for reverse_string.js

--- keep_hydrated.js
+++ keep_hydrated.js
@@ -1,8 +1,3 @@
 function litres(time) {
-  let litres = 0;
-  const litreEachHalfHour = 0.5;
-
-  litres = Math.floor(time * litreEachHalfHour);
-  return litres;
-
+ return Math.floor(time * 0.5);
 }
