ea1d9200b098ada583507ab7bad82986017c2255
MavFeuille <mavis.feuille@gmail.com>
2021-11-11 10:26:20 +0800

    Remove redundant comments

--- area_of_a_square.js
+++ area_of_a_square.js
@@ -1,17 +1,3 @@
 const squareArea = (A) => {
-  // let radius = 0;
-  // let squareArea = 0;
-
-  // //circumfrance
-  // let circumfrance = 4 * A
-
-  // circumfrance = 2 * Math.PI * radius;
-  // radius = circumrance / 2 * Math.PI
-  //        = 4 * A / 2 * Math.PI;
-  //        = 2 * A / Math.PI
-
   return Number(((2 * A / Math.PI) ** 2).toFixed(2))
-
 }
-
-console.log(squareArea(14.05));
