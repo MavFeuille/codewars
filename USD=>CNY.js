4293cadb816296a4dfe89972c80d8227d11fd064
MavFeuille <mavis.feuille@gmail.com>
2021-11-15 23:13:23 +0800

    Refactor

--- USD=>CNY.js
+++ USD=>CNY.js
@@ -1,9 +1,3 @@
 const usdcny = usd => {
-  const rate = 6.75;
-  let cny = 0;
-  cny = (usd * rate);
-  
-  return `${cny.toFixed(2).toString()} Chinese Yuan`;
+ return `${(usd * 6.75).toFixed(2).toString()} Chinese Yuan`
 }
-
-// console.log(usdcny(15));
