cb30fd6607fa5feb31cda81247c9e773178fc560
MavFeuille <mavis.feuille@gmail.com>
2021-11-18 22:37:14 +0800

    Create squareSum function

---
+++ square(n)sum.js
@@ -0,0 +1,11 @@
+const squareSum = num => {
+  let sum = 0;
+
+  for (number of num) {
+    sum += number ** 2
+    // console.log("sum: ", sum);
+  }
+  return sum
+}
+
+console.log(squareSum([1,2]));
