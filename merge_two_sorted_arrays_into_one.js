fc64e2e6b5f111a9b686a48f4ba76764d1f0e7a8
MavFeuille <mavis.feuille@gmail.com>
2021-11-19 22:59:47 +0800

    Create mergeArrays function

--- merge_two_sorted_arrays_into_one.js
+++ merge_two_sorted_arrays_into_one.js
@@ -2,7 +2,5 @@ const mergeArrays = (arr1, arr2) => {
   let newArr = arr1.concat(arr2);
  
   newArr.sort((a, b) => a - b);
-  return newArr;
+  return Array.from(new Set(newArr));
 }
-
-console.log(mergeArrays([1,3,5,7,9], [10,8,6,4,2]));
