c6b2dd5f8fc9042d5ad306bed5573fb162357ccf
MavFeuille <mavis.feuille@gmail.com>
2021-11-21 21:25:36 +0800

    Refactor

--- sum_of_positives_negatives.js
+++ sum_of_positives_negatives.js
@@ -12,9 +12,6 @@ const countPositivesSumNegatives = (input) => {
     }
   }
   return [positives.length, sumOfNegative];
-  // console.log("positives: ", positives);
-  // console.log("positives.length: ", positives.length);
-  // console.log("sumOfNegative: ", sumOfNegative);
 }
 
 console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
