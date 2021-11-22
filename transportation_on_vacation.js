d406832575784aa240a7094a654747a1fb71d4ab
MavFeuille <mavis.feuille@gmail.com>
2021-11-22 22:57:56 +0800

    Create rentalCarCost function

---
+++ transportation_on_vacation.js
@@ -0,0 +1,13 @@
+const rentalCarCost = (d) => {
+  const costPerDay = 40;
+  let totalCost = 0;
+
+  if (d >= 3 && d < 7) {
+    return totalCost = d * costPerDay - 20;
+  } else if (d >= 7) {
+    return totalCost = d * costPerDay - 50;
+  } else {
+    return totalCost = d * costPerDay;
+  }
+}
+console.log(rentalCarCost(10));
