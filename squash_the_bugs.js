6ca92f47d0b6fdd4feab4b70f44c4d40086b8def
MavFeuille <mavis.feuille@gmail.com>
2021-11-22 17:45:23 +0800

    Initial commit

---
+++ squash_the_bugs.js
@@ -0,0 +1,15 @@
+function findLongest(str) {
+  
+  let spl = str.split(" ");
+  let longest = 0
+  
+  for (let i = 0; i < spl.length; i++) {
+   
+    if (spl[i].length > longest) {
+      longest = spl[i].length
+    }
+  }
+    return longest
+}
+
+console.log(findLongest("Take me to tinseltown with you"));
