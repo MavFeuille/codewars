302048892d0a582c79dbeb9e9b43ac1c43f1d8e2
MavFeuille <mavis.feuille@gmail.com>
2021-11-23 12:36:29 +0800

    Refractor

--- parse_nice_int_from_char_problem.js
+++ parse_nice_int_from_char_problem.js
@@ -1,5 +1,3 @@
 const getAge = inputString => {
-  // console.log(Number(inputString.charAt(0)));
-  return Number(inputString.charAt(0))
+   return Number(inputString.charAt(0))
 }
-getAge("4 years old");
