3ee09e70eb2bd66a9b1fa16173f11da920cd1d60
MavFeuille <mavis.feuille@gmail.com>
2021-11-23 13:43:17 +0800

    Refactor

--- calculate_bmi.js
+++ calculate_bmi.js
@@ -1,15 +1,5 @@
 const bmi = (weight, height) => {
-  let bmi = weight/height ** 2;
+  const bmi = weight/Math.pow(height, 2);
 
-  if (bmi <= 18.5) {
-    return "Underweight";
-  } else if (bmi <= 25.0) {
-    return "Normal";
-  } else if (bmi <= 30.0) {
-    return "Overweight";
-  } else {
-    return "Obese"
-  }
+  return bmi <= 18.5 ? "Underweight" : bmi <= 25.0 ? "Normal" : bmi <= 30.0 ? "Overweight" : "Obese";
 }
-
-console.log(bmi(80, 1.80));
