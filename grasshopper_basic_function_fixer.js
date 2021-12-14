2fec858222862bbddfe27a48285a42428b65c08c
MavFeuille <mavis.feuille@gmail.com>
2021-12-15 01:30:58 +0800

    Refactor function to function expression

--- grasshopper_basic_function_fixer.js
+++ grasshopper_basic_function_fixer.js
@@ -1,6 +1,12 @@
-function addFive(num) {
+// function addFive(num) {
+//   var total = num + 5
+//   return total;
+// }
+
+const addFive = (num) => {
   var total = num + 5
   return total;
 }
+
 
 console.log(addFive(5));
