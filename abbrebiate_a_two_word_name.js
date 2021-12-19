192a806b95c44b37be31d0a6f9f85e3c1be00c73
MavFeuille <mavis.feuille@gmail.com>
2021-12-20 01:41:40 +0800

    Set up variable

--- abbrebiate_a_two_word_name.js
+++ abbrebiate_a_two_word_name.js
@@ -1,3 +1,10 @@
+const name = "Sam Harris";
+
 const abrevName = (name) => {
-  
+  const splitName = name.split(" ");
+  console.log("🚀 ~ file: abbrebiate_a_two_word_name.js ~ line 3 ~ abrevName ~ splitName", splitName)
+
+  let output = splitName.map(() => )
 }
+
+console.log(abrevName(name))
