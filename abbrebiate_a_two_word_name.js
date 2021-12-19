f7a0b989901a6af8463cc8bea6d1bf1095bd9e5c
MavFeuille <mavis.feuille@gmail.com>
2021-12-20 01:47:06 +0800

    Comment out output

--- abbrebiate_a_two_word_name.js
+++ abbrebiate_a_two_word_name.js
@@ -1,12 +1,12 @@
 const name = "Sam Harris";
 
 const abbrevName = (name) => {
-  const splitName = name.split(" ");
-  // console.log("🚀 ~ file: abbrebiate_a_two_word_name.js ~ line 3 ~ abrevName ~ splitName", splitName)
+  return  name.split(" ").map((word) => word[0]).join(".");
+  // console.log("🚀 ~ file: abbrebiate_a_two_word_name.js ~ line 5 ~ abbrevName ~ splitName", splitName)
 
-  let output = splitName.map((word) => word[0]).join(".")
-  return 
-  console.log("🚀 ~ file: abbrebiate_a_two_word_name.js ~ line 8 ~ abrevName ~ output", output)
+  // let output = splitName.map((word) => word[0]).join(".")
+  // return splitName
+  // console.log("🚀 ~ file: abbrebiate_a_two_word_name.js ~ line 8 ~ abrevName ~ output", output)
 }
 
-console.log(abrevName(name))
+console.log(abbrevName(name));
