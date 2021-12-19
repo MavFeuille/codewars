e676def9ae5f05933259083f0febe0b76384ed58
MavFeuille <mavis.feuille@gmail.com>
2021-12-20 01:44:55 +0800

    change function name to abbrevName

--- abbrebiate_a_two_word_name.js
+++ abbrebiate_a_two_word_name.js
@@ -1,10 +1,12 @@
 const name = "Sam Harris";
 
-const abrevName = (name) => {
+const abbrevName = (name) => {
   const splitName = name.split(" ");
-  console.log("🚀 ~ file: abbrebiate_a_two_word_name.js ~ line 3 ~ abrevName ~ splitName", splitName)
+  // console.log("🚀 ~ file: abbrebiate_a_two_word_name.js ~ line 3 ~ abrevName ~ splitName", splitName)
 
-  let output = splitName.map(() => )
+  let output = splitName.map((word) => word[0]).join(".")
+  return 
+  console.log("🚀 ~ file: abbrebiate_a_two_word_name.js ~ line 8 ~ abrevName ~ output", output)
 }
 
 console.log(abrevName(name))
