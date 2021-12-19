c4a15e823ec41d3b2028339c4479008c2fe72b16
MavFeuille <mavis.feuille@gmail.com>
2021-12-20 01:47:45 +0800

    Remove comments

--- abbrebiate_a_two_word_name.js
+++ abbrebiate_a_two_word_name.js
@@ -2,11 +2,5 @@ const name = "Sam Harris";
 
 const abbrevName = (name) => {
   return  name.split(" ").map((word) => word[0]).join(".");
-  // console.log("🚀 ~ file: abbrebiate_a_two_word_name.js ~ line 5 ~ abbrevName ~ splitName", splitName)
-
-  // let output = splitName.map((word) => word[0]).join(".")
-  // return splitName
-  // console.log("🚀 ~ file: abbrebiate_a_two_word_name.js ~ line 8 ~ abrevName ~ output", output)
 }
-
 console.log(abbrevName(name));
