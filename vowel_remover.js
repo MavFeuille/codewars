ec58a495c21fd9a7c338b471df39790e0be5d3bb
MavFeuille <mavis.feuille@gmail.com>
2021-12-20 01:35:25 +0800

    comment out to restart

--- vowel_remover.js
+++ vowel_remover.js
@@ -1,5 +1,15 @@
 const shortcut = (string) => {
-  let newString = "";
+  // let newString = "";
+  const vowels = ["a", "e", "i", "o", "u"];
+  // // const vowels = "a" || "e" || "i" || "o" || "u";
+  // // console.log("vowels: ", vowels)
+  
+  // for (let i = 0; i < string.length; i ++) {
+  //   if (string[i] === "a" || "e" || "i" || "o" || "u") {
+  //     console.log(string[i], true)
+  //   }
+  // }
 
   
 }
+console.log(shortcut('hello'));
