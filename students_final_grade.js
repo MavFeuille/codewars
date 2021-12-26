e945a2c642abed30592ac33bd2f4fa3e9ef61bb2
MavFeuille <mavis.feuille@gmail.com>
2021-12-25 22:19:03 -0500

    Update grade 75 requirement

--- students_final_grade.js
+++ students_final_grade.js
@@ -1,15 +1,16 @@
 const finalGrade = (exams, projects) => {
-  if (exams >= 90 || projects >= 10) {
-    return 100;
-  } else if (exams >= 75 && exams < 90 && projects >=5  && projects < 10) {
-    return 90;
-  } else if (exams > 50 && exams < 75 && projects >= 2 && projects < 5) {
-    return 75;
-  } else {
-    return 0;
-  }
+  // if (exams >= 90 || projects >= 10) {
+  //   return 100;
+  // } else if (exams >= 75 && exams < 90 && projects >=5  && projects < 10) {
+  //   return 90;
+  // } else if (exams >= 50 && exams < 75 && projects >= 2 && projects < 5) {
+  //   return 75;
+  // } else {
+  //   return 0;
+  // }
 
+  return (exams >= 90 || projects >= 10) ? 100 : (exams >= 75 && exams < 90 && projects >=5  && projects < 10) ? 90 : (exams >= 50 && exams < 75 && projects >= 2 && projects < 5) ? 75 : 0 ;
   
 }
 
-console.log(finalGrade(85, 5));
+console.log(finalGrade(50, 4));
