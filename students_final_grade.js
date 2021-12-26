aefeac23c19c4e5b431904d6922a807907397f7e
MavFeuille <mavis.feuille@gmail.com>
2021-12-25 22:15:49 -0500

    Create finalGrade function

--- students_final_grade.js
+++ students_final_grade.js
@@ -1,3 +1,13 @@
 const finalGrade = (exams, projects) => {
-  
+  if (exams >= 90 || projects >= 10) {
+    return 100;
+  } else if (exams >= 75 && exams < 90 && projects >=5  && projects < 10) {
+    return 90;
+  } else if (exams > 50 && exams < 75 && projects >= 2 && projects < 5) {
+    return 75;
+  } else {
+    return 0;
+  }
 }
+
+console.log(finalGrade(100, 12));
