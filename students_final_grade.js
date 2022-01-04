713959c6fb1672a8d83f71556db69b71ecb243be
MavFeuille <mavis.feuille@gmail.com>
2022-01-03 23:25:45 -0500

    Update whole function

--- students_final_grade.js
+++ students_final_grade.js
@@ -1,16 +1,14 @@
 const finalGrade = (exams, projects) => {
-  // if (exams >= 90 || projects >= 10) {
-  //   return 100;
-  // } else if (exams >= 75 && exams < 90 && projects >=5  && projects < 10) {
-  //   return 90;
-  // } else if (exams >= 50 && exams < 75 && projects >= 2 && projects < 5) {
-  //   return 75;
-  // } else {
-  //   return 0;
-  // }
-
-  return (exams >= 90 || projects >= 10) ? 100 : (exams >= 75 && exams < 90 && projects >=5  && projects < 10) ? 90 : (exams >= 50 && exams < 75 && projects >= 2 && projects < 5) ? 75 : 0 ;
+  if (exams > 90 || projects > 10) {
+    return 100;
+  } else if (exams > 75 && projects >= 5) {
+    return 90;
+  } else if (exams > 50 && projects >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
   
 }
 
-console.log(finalGrade(50, 4));
+console.log(finalGrade(99, 0));
