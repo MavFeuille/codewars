b702b5231d22e7e2cfafb3c4993cf741da65e637
MavFeuille <mavis.feuille@gmail.com>
2022-01-03 23:30:48 -0500

    Refactor

--- students_final_grade.js
+++ students_final_grade.js
@@ -1,14 +1,18 @@
 const finalGrade = (exams, projects) => {
-  if (exams > 90 || projects > 10) {
-    return 100;
-  } else if (exams > 75 && projects >= 5) {
-    return 90;
-  } else if (exams > 50 && projects >= 2) {
-    return 75;
-  } else {
-    return 0;
-  }
-  
+  // if (exams > 90 || projects > 10) {
+  //   return 100;
+  // } else if (exams > 75 && projects >= 5) {
+  //   return 90;
+  // } else if (exams > 50 && projects >= 2) {
+  //   return 75;
+  // } else {
+  //   return 0;
+  // }
+
+  if (exams > 90 || projects > 10) return 100;
+  if (exams > 75 && projects >= 5) return 90;
+  if (exams > 50 && projects >= 2) return 75;
+  return 0;
 }
 
-console.log(finalGrade(99, 0));
+console.log(finalGrade(100, 2));
