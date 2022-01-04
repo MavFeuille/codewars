5082ad75ce79f28bfa04e1199f53282adc6d774f
MavFeuille <mavis.feuille@gmail.com>
2022-01-03 23:30:58 -0500

    Remove comments

--- students_final_grade.js
+++ students_final_grade.js
@@ -1,14 +1,4 @@
 const finalGrade = (exams, projects) => {
-  // if (exams > 90 || projects > 10) {
-  //   return 100;
-  // } else if (exams > 75 && projects >= 5) {
-  //   return 90;
-  // } else if (exams > 50 && projects >= 2) {
-  //   return 75;
-  // } else {
-  //   return 0;
-  // }
-
   if (exams > 90 || projects > 10) return 100;
   if (exams > 75 && projects >= 5) return 90;
   if (exams > 50 && projects >= 2) return 75;
