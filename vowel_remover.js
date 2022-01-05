e3a03283f48261ef2316aaae2aa61db41f521d73
MavFeuille <mavis.feuille@gmail.com>
2022-01-04 23:02:41 -0500

    Remove console.log

--- vowel_remover.js
+++ vowel_remover.js
@@ -2,15 +2,10 @@ const shortcut = (string) => {
   let newString = string.split("");
   
   for (let i = 0; i < newString.length; i ++) {
-    if (newString[i] === "a" || newString[i] === "e" || newString[i] === "i" || newString[i] === "o" || newString[i] === "u") {
-      // console.log(newString[i], true)
+    if (newString[i] === "a" || newString[i] === "e" || newString[i] === "i" || newString[i] === "o" || newString[i] === "u") {  
       newString[i]= "";
     }
-    // console.log("newString in for loop: ", newString);
-    // console.log(newString.join(""));
   }
   return newString.join("");
-
-  
 }
 console.log(shortcut('hello'));
