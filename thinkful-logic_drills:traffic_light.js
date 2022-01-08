d9be82360944f5285fa7ad5ce9a2bb9d7148fe47
MavFeuille <mavis.feuille@gmail.com>
2022-01-08 16:29:43 -0500

    Create function to update traffic light

--- thinkful-logic_drills:traffic_light.js
+++ thinkful-logic_drills:traffic_light.js
@@ -1,5 +1,13 @@
 const updateLight = (current) => {
-
+  if (current === "green") {
+    return "yellow";
+  } else if (current === "yellow") {
+    return "red";
+  } else {
+    return "green";
+  }
 }
 
 console.log(updateLight("green"));
+console.log(updateLight("yellow"));
+console.log(updateLight("red"));
