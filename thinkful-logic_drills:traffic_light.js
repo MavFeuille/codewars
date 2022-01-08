c0880e21ce29c8f49cb4a7bc9e1b47a8175c7868
MavFeuille <mavis.feuille@gmail.com>
2022-01-08 16:35:19 -0500

    Refactor if statement

--- thinkful-logic_drills:traffic_light.js
+++ thinkful-logic_drills:traffic_light.js
@@ -1,13 +1,6 @@
 const updateLight = (current) => {
 
   return current === "red" ? "green" : current === "green" ? "yellow" : "red"; 
-  // if (current === "green") {
-  //   return "yellow";
-  // } else if (current === "yellow") {
-  //   return "red";
-  // } else {
-  //   return " green";
-  // }
 }
 
 console.log(updateLight("green"));
