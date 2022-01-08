3d8cc4806867d8068581f061a205c98502a7f0d6
MavFeuille <mavis.feuille@gmail.com>
2022-01-07 21:15:24 -0500

    First commit

---
+++ well_of_ideas_easy_version.js
@@ -0,0 +1,16 @@
+const well = (x) => {
+  let countBad = 0;
+  let countGood = 0;
+
+  for (const item of x) {
+    if (item === 'bad') {
+      countBad += 1;
+    } else {
+      countGood += 1;
+    }
+  }
+  console.log('countBad', countBad);
+  console.log('countGood', countGood);
+}
+
+console.log(well(['bad', 'bad', 'bad']));
