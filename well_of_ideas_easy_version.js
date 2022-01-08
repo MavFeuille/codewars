4b1241e4f79abdb2b098ebdfec572a154f50a08e
MavFeuille <mavis.feuille@gmail.com>
2022-01-07 21:21:08 -0500

    Create function named well to determine good or bad ideas

--- well_of_ideas_easy_version.js
+++ well_of_ideas_easy_version.js
@@ -7,9 +7,15 @@ const well = (x) => {
     }
   }
 
-  if (countGood > 0)
-  // console.log('countBad', countBad);
-  // console.log('countGood', countGood);
+  if (countGood <= 0){
+    return "Fail";
+  } else if (countGood  > 2) {
+    return "I smell a series!";
+  } else {
+    return "Publish!"
+  }
 }
 
 console.log(well(['bad', 'bad', 'bad']));
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad']));
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']));
