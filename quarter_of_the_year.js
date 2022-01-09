2c74f915f268e987664f4c01d1be74776048d736
MavFeuille <mavis.feuille@gmail.com>
2022-01-09 13:02:52 -0500

    Refactor if statement

--- quarter_of_the_year.js
+++ quarter_of_the_year.js
@@ -2,17 +2,6 @@ const quarterOf = (month) => {
 
   return month < 4 ? 1 : month > 3 && month < 7 ? 2 : month > 6 && month < 10 ? 3 : 4;
 
-  // let quarter = 0;
-
-  // if (month <= 3) {
-  //   return quarter = 1;
-  // } else if (month > 3 && month <= 6) {
-  //   return quarter = 2;
-  // } else if (month > 6 && month <= 9) {
-  //   return quarter = 3;
-  // } else {
-  //   return quarter = 4;
-  // }
 }
 
 console.log(quarterOf(3));
