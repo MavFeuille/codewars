a2c14f917c372b422c9f943bd3afb409aa9d31b2
MavFeuille <mavis.feuille@gmail.com>
2022-01-09 13:18:12 -0500

    Refactor if statment

--- grasshopper_grade_book.js
+++ grasshopper_grade_book.js
@@ -2,17 +2,7 @@ const getGrade = (s1, s2, s3) => {
   let average = (s1 + s2 + s3) / 3;
   
   return average >= 90 ? 'A' : average >= 80 ? 'B' : average >= 70 ? 'C' : average >= 60 ? 'D' : 'F' ;
-  // if (average >= 90) {
-  //   return 'A';
-  // } else if (average >= 80) {
-  //   return 'B';
-  // } else if (average >= 70) {
-  //   return 'C';
-  // } else if (average >= 60) {
-  //   return 'D';
-  // } else {
-  //   return 'F';
-  // }
+ 
 }
 
 console.log(getGrade(100,85,96));
