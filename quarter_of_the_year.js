f0f1e310ce1f106037d7dc62fd721d9059c48d34
MavFeuille <mavis.feuille@gmail.com>
2022-01-09 12:58:02 -0500

    Create function to check quarter of the year

--- quarter_of_the_year.js
+++ quarter_of_the_year.js
@@ -1,3 +1,13 @@
 const quarterOf = (month) => {
-  
+  let quarter = 0;
+
+  if (month <= 3) {
+    return quarter = 1;
+  } else if (month > 3 && month <= 6) {
+    return quarter = 2;
+  } else if (month > 6 && month <= 9) {
+    return quarter = 3;
+  } else {
+    return quarter = 4;
+  }
 }
