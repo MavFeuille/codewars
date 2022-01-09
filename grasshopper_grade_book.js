255271c342fbd3e0f53b421738d6732585693887
MavFeuille <mavis.feuille@gmail.com>
2022-01-09 13:16:01 -0500

    Add test cases

--- grasshopper_grade_book.js
+++ grasshopper_grade_book.js
@@ -13,3 +13,9 @@ const getGrade = (s1, s2, s3) => {
     return 'F';
   }
 }
+
+console.log(getGrade(100,85,96));
+console.log(getGrade(89,89,90));
+console.log(getGrade(60,82,76));
+console.log(getGrade(66,62,68));
+console.log(getGrade(58,59,60));
