dc3351c39441482675e17a8e2c59db0347a335b1
MavFeuille <mavis.feuille@gmail.com>
2022-01-09 13:05:13 -0500

    Remove redundant code in if statement

--- quarter_of_the_year.js
+++ quarter_of_the_year.js
@@ -1,5 +1,5 @@
 const quarterOf = (month) => {
-  return month < 4 ? 1 : month > 3 && month < 7 ? 2 : month > 6 && month < 10 ? 3 : 4;
+  return month < 4 ? 1 : month < 7 ? 2 : month < 10 ? 3 : 4;
 }
 
 console.log(quarterOf(3));
