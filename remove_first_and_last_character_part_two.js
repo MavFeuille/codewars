5a7ccd3d6f93b1a587568c06371a5c94f13a9a3f
MavFeuille <mavis.feuille@gmail.com>
2022-01-11 17:26:02 -0500

    Create function to remove first and last characters

--- remove_first_and_last_character_part_two.js
+++ remove_first_and_last_character_part_two.js
@@ -2,7 +2,12 @@ const array = (arr) => {
   if (arr.length <= 2) {
     return null;
   } else {
-    return arr.slice(1, -2);
+   let characters = arr.split(',')
+  //  console.log("🚀 ~ file: remove_first_and_last_character_part_two.js ~ line 6 ~ array ~ characters", characters)
+   characters.shift();
+   characters.pop();
+  //  console.log(characters);
+    return characters;
   }
 }
 
