f2f3037f134b81b52131dd4e4f5c6edab5158657
MavFeuille <mavis.feuille@gmail.com>
2022-01-10 20:00:52 -0500

    Remove comments and white spaces

--- will_there_be_enough_space?.js
+++ will_there_be_enough_space?.js
@@ -1,10 +1,4 @@
 const enough = (cap, on, wait) => {
-  // if (cap >= on + wait) {
-  //   return 0;
-  // } else {
-  //   return Math.abs(cap - on - wait);
-  // }
-
   return cap >= on + wait ? 0 : Math.abs(cap - on - wait);
 }
 
