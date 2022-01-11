a873a9b306fc5ff4e0443c752cbc728b1be622cd
MavFeuille <mavis.feuille@gmail.com>
2022-01-11 17:32:16 -0500

    Add more test cases to return null

--- remove_first_and_last_character_part_two.js
+++ remove_first_and_last_character_part_two.js
@@ -1,14 +1,17 @@
 const array = (arr) => {
-  if (arr.length <= 2) {
+  if (arr.length < 3 || arr.length === null) {
     return null;
   } else {
    let characters = arr.split(',')
   //  console.log("🚀 ~ file: remove_first_and_last_character_part_two.js ~ line 6 ~ array ~ characters", characters)
    characters.shift();
    characters.pop();
-  //  console.log(characters);
-    return characters;
+  //  console.log(characters.join(' '));
+    return characters.join(' ');
   }
 }
 
 console.log(array('1,2,3,4,5'));
+console.log(array(''));
+console.log(array('1'));
+console.log(array('1, 2'));
