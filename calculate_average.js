3d1ff23403ff0970fc55affeb739f46f8bdfa91a
MavFeuille <mavis.feuille@gmail.com>
2022-01-11 16:42:22 -0500

    Correct file name

--- calculate_average.js
+++ calculate_average.js
@@ -1,16 +1,11 @@
 const find_average = (array) => {
-  if (array.length === 0) {
-    return 0;
-  };
-
-  let average = 0;
+  if (array.length === 0) return 0;
+  
   let sum = 0;
-
   for (const item of array) {
     sum += item;
   }
-  average = sum / array.length;
-  return average;
+  return sum/
 }
 
 console.log(find_average([1, 2, 3]));
