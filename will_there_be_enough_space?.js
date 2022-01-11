7eb52aeff37bfea28269be737094bc28f53e8624
MavFeuille <mavis.feuille@gmail.com>
2022-01-10 19:57:44 -0500

    Continue if statement and add test case

--- will_there_be_enough_space?.js
+++ will_there_be_enough_space?.js
@@ -1,7 +1,10 @@
 const enough = (cap, on, wait) => {
-  if (cap <= on + wait) {
+  if (cap >= on + wait) {
     return 0;
+  } else {
+    return cap - on - wait;
   }
 }
 
-console.log(enough(10, 5, 5));
+console.log(enough(20, 5, 5));
+console.log(enough(100, 60, 50));
