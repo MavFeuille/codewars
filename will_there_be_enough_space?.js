a1aff9de79d6b777dc130c8c80d6f1eee8550b15
MavFeuille <mavis.feuille@gmail.com>
2022-01-10 19:59:34 -0500

    Refactor if statement

--- will_there_be_enough_space?.js
+++ will_there_be_enough_space?.js
@@ -1,9 +1,11 @@
 const enough = (cap, on, wait) => {
-  if (cap >= on + wait) {
-    return 0;
-  } else {
-    return Math.abs(cap - on - wait);
-  }
+  // if (cap >= on + wait) {
+  //   return 0;
+  // } else {
+  //   return Math.abs(cap - on - wait);
+  // }
+
+  return cap >= on + wait ? 0 : Math.abs(cap - on - wait);
 }
 
 console.log(enough(20, 5, 5));
