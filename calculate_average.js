3dbe37945d6544ef47b1921ce570363da6257a55
MavFeuille <mavis.feuille@gmail.com>
2022-01-11 16:34:53 -0500

    First commit

---
+++ calculate_average.js
@@ -0,0 +1,12 @@
+const find_average = (array) => {
+  let average = 0;
+  let sum = 0;
+
+  for (const item of array) {
+    sum += item;
+  }
+  average = sum / array.length;
+
+  return average;
+}
+
