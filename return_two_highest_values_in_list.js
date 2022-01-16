2f5ea53f564d8ca694f3367b64b19f856ca83f46
MavFeuille <mavis.feuille@gmail.com>
2022-01-15 22:07:44 -0500

    Set up for-loop to find the two highest value

--- return_two_highest_values_in_list.js
+++ return_two_highest_values_in_list.js
@@ -1,5 +1,17 @@
 const twoHighest = arr => {
   arr.sort()
+  console.log(arr)
+
+  let highest = 0;
+  let secondHighest = 0;
+
+  for (let i = 0; i < arr.length; i ++) {
+    if (arr[i] > highest) {
+      highest = arr[i];
+    } else if (arr[i] > secondHighest && arr[i] < highest) {
+      secondHighest = arr[i];
+    }
+  }
 }
 
-console.log(arr([15, 20, 20, 17]))
+console.log(twoHighest([15, 20, 20, 17]))
