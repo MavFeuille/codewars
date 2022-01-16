46e9a48d72e99943436f7c9a23a257f3efc600c5
MavFeuille <mavis.feuille@gmail.com>
2022-01-15 21:57:56 -0500

    Turn argument to string, split and reverse

--- convert_number_to_reversed_array_of_digits.js
+++ convert_number_to_reversed_array_of_digits.js
@@ -1,6 +1,8 @@
 const digitize = n => {
   
-
+  n.toString().split('').reverse()
+  console.log("🚀 ~ file: convert_number_to_reversed_array_of_digits.js ~ line 4 ~  n.toString().split('').reverse()",  n.toString().split('').reverse())
+  
  
   
 }
