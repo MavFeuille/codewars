4419e65503efcd74f5e4ed6b2cf0d5eec591d0c7
MavFeuille <mavis.feuille@gmail.com>
2022-01-15 21:59:54 -0500

    Remove comments and white space

--- convert_number_to_reversed_array_of_digits.js
+++ convert_number_to_reversed_array_of_digits.js
@@ -1,11 +1,5 @@
 const digitize = n => {
-  
   return n.toString().split('').reverse().map(Number)
-  // console.log("🚀 ~ file: convert_number_to_reversed_array_of_digits.js ~ line 4 ~ n.toString().split('').reverse().map(Number)", n.toString().split('').reverse().map(Number))
-  // console.log("🚀 ~ file: convert_number_to_reversed_array_of_digits.js ~ line 4 ~  n.toString().split('').reverse()",  n.toString().split('').reverse())
-  
- 
-  
 }
 
 console.log(digitize(348597));
