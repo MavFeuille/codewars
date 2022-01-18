5cfd5e214a0058adee196c3c3ed49677506a575f
MavFeuille <mavis.feuille@gmail.com>
2022-01-17 22:39:07 -0500

    Remove comments and white spaces

--- pirates!!are_the_cannons_ready.js
+++ pirates!!are_the_cannons_ready.js
@@ -2,14 +2,7 @@ const a = {'Mike':'aye','Joe':'aye','Johnson':'aye','Peter':'aye'},
       b = {'Mike':'aye','Joe':'nay','Johnson':'aye','Peter':'aye'};
 
  cannonsReady = (gunners) => {
-  let responsesArray = Object.values(gunners);
-
   return Object.values(gunners).includes('nay') ? 'Shiver me timbers!' : 'Fire!'
-  // if (responsesArray.includes('nay')) {
-  //   return 'Shiver me timbers!'
-  // } else {
-  //   return 'Fire!'
-  // }
 }
 
 console.log(cannonsReady(a));
