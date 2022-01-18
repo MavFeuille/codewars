917e2a3d7ad51a5d6b1207341982cb2aef0a09d1
MavFeuille <mavis.feuille@gmail.com>
2022-01-17 22:35:24 -0500

    Extract gunners' value to be an array form

--- pirates!!are_the_cannons_ready.js
+++ pirates!!are_the_cannons_ready.js
@@ -2,14 +2,10 @@ const a = {'Mike':'aye','Joe':'aye','Johnson':'aye','Peter':'aye'},
       b = {'Mike':'aye','Joe':'nay','Johnson':'aye','Peter':'aye'};
 
  cannonsReady = (gunners) => {
-  for (prop in gunners) {
-    if (gunners[prop] === 'nay') {
-      return 'Shiver me timbers!'
-    } else {
-      return 'Fire!'
-    }
-  }
+  let responseArray = Object.values(gunners);
+  // console.log("🚀 ~ file: pirates!!are_the_cannons_ready.js ~ line 6 ~ responseArray", responseArray)
+  
 }
 
-console.log(cannonsReady(a));
+// console.log(cannonsReady(a));
 console.log(cannonsReady(b))
