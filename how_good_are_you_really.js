dc29881b6f1eb4672d2e49fabf52565442139389
MavFeuille <mavis.feuille@gmail.com>
2022-01-17 22:05:58 -0500

    Set variable for average value

--- how_good_are_you_really.js
+++ how_good_are_you_really.js
@@ -2,6 +2,8 @@ const betterThanAverage = (classPoints, yourPoints) => {
   let newArr = [...classPoints]
   newArr.push(yourPoints)
 
+  let average = newArr.reduce((a, b) => a + b) / newArr.length
+  // console.log("🚀 ~ file: how_good_are_you_really.js ~ line 6 ~ betterThanAverage ~ average", average)
 
 }
 
