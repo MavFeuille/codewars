948900e7ed74b4e5b252ed6c87c89fff389be299
MavFeuille <mavis.feuille@gmail.com>
2022-01-17 22:10:31 -0500

    Refactor

--- how_good_are_you_really.js
+++ how_good_are_you_really.js
@@ -1,14 +1,16 @@
 const betterThanAverage = (classPoints, yourPoints) => {
-  let newArr = [...classPoints]
-  newArr.push(yourPoints)
+  // let newArr = [...classPoints]
+  // newArr.push(yourPoints)
 
-  let average = newArr.reduce((a, b) => a + b) / newArr.length
-  // console.log("🚀 ~ file: how_good_are_you_really.js ~ line 6 ~ betterThanAverage ~ average", average)
-  if (yourPoints > average) {
-    return true;
-  } else {
-    return false;
-  }
+  // let average = newArr.reduce((a, b) => a + b) / newArr.length
+  // // console.log("🚀 ~ file: how_good_are_you_really.js ~ line 6 ~ betterThanAverage ~ average", average)
+  // if (yourPoints > average) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
+  return yourPoints > classPoints.reduce((a, b) => a + b) / classPoints.length
 }
 
 console.log(betterThanAverage([2, 3], 5));
