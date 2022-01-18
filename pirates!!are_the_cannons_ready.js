1b42cf30f0b59ab22522bac598a94e73dff2267f
MavFeuille <mavis.feuille@gmail.com>
2022-01-17 22:25:32 -0500

    Create function to check if cannons are ready

--- pirates!!are_the_cannons_ready.js
+++ pirates!!are_the_cannons_ready.js
@@ -1,3 +1,9 @@
 const cannonsReady = (gunners) => {
-  
+  for (prop in gunners) {
+    if (gunners[prop] === 'nay') {
+      return 'Shiver me timbers!'
+    } else {
+      return 'Fire!'
+    }
+  }
 }
