79698827a6d53410222c37392dec093afcf137bd
MavFeuille <mavis.feuille@gmail.com>
2022-01-18 22:26:46 -0500

    Refactor

--- fake_binary.js
+++ fake_binary.js
@@ -1,14 +1,16 @@
 const fakeBin = (x) => {
-  let output = '';
+  // let output = '';
 
-  for (let i = 0; i < x.length; i++) {
-    if (x[i] < '5') {
-      output += '0';
-    } else {
-      output += '1'
-    }
-  }
-  return output;
+  // for (let i = 0; i < x.length; i++) {
+  //   if (x[i] < '5') {
+  //     output += '0';
+  //   } else {
+  //     output += '1'
+  //   }
+  // }
+  // return output;
+
+  return x.replace(/[1234]/g, '0').replace(/[56789]/g,'1');
 }
 
 console.log(fakeBin('45385593107843568'));
