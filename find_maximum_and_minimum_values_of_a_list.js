9d6985c5b53ccdaad32f10a64a46a3656a26d6e5
MavFeuille <mavis.feuille@gmail.com>
2022-01-18 22:45:17 -0500

    Complete min and max function with spread syntax

--- find_maximum_and_minimum_values_of_a_list.js
+++ find_maximum_and_minimum_values_of_a_list.js
@@ -1,9 +1,10 @@
 const min = (list) => {
-  return Math.min(list);
+ 
+  return Math.min(...list);
 };
 
 const max = (list) => {
-  return Math.max(list);
+  return Math.max(...list);
 };
 
 console.log(min([-52, 56, 30, 29, -54, 0, -110]));
