22fbefecb1cb6c0cad7ad1cab3d69272f9336eb8
MavFeuille <mavis.feuille@gmail.com>
2022-01-18 22:05:50 -0500

    Turn argument to array form

--- fake_binary.js
+++ fake_binary.js
@@ -1,6 +1,8 @@
 const fakeBin = (x) => {
-  let xNum = Number(x);
-  console.log("🚀 ~ file: fake_binary.js ~ line 3 ~ fakeBin ~ xNum", xNum)
+  let xNum = Array.from(x);
+  // console.log("🚀 ~ file: fake_binary.js ~ line 3 ~ fakeBin ~ xNum", xNum)
+
+  
 }
 
 console.log(fakeBin('45385593107843568'));
