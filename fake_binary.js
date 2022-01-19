a1e02c86cf756620e42c516e0da4ea14a7bf9e3f
MavFeuille <mavis.feuille@gmail.com>
2022-01-18 22:23:06 -0500

    Create function fakeBin to turn string to binary form

--- fake_binary.js
+++ fake_binary.js
@@ -1,5 +1,14 @@
 const fakeBin = (x) => {
-  
+  let output = '';
+
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] < '5') {
+      output += '0';
+    } else {
+      output += '1'
+    }
+  }
+  console.log(output)
 }
 
 console.log(fakeBin('45385593107843568'));
