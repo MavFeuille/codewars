74f1b964e6d538551d90ee1520dd82945b67ece8
MavFeuille <mavis.feuille@gmail.com>
2022-01-18 22:27:22 -0500

    Remove comments

--- fake_binary.js
+++ fake_binary.js
@@ -1,15 +1,4 @@
 const fakeBin = (x) => {
-  // let output = '';
-
-  // for (let i = 0; i < x.length; i++) {
-  //   if (x[i] < '5') {
-  //     output += '0';
-  //   } else {
-  //     output += '1'
-  //   }
-  // }
-  // return output;
-
   return x.replace(/[1234]/g, '0').replace(/[56789]/g,'1');
 }
 
